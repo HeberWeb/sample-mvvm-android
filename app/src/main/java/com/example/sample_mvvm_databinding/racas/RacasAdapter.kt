821b106952c4be1e823dd2ecca1c8516f711cbf6
Heber Nickison Pietrafeza <hpietrafeza@autodoc.com.br>
2019-09-23 17:59:43 -0300

    realizando teste e entendendo databinding

--- app/src/main/java/com/example/sample_mvvm_databinding/racas/RacasAdapter.kt
+++ app/src/main/java/com/example/sample_mvvm_databinding/racas/RacasAdapter.kt
@@ -13,12 +13,14 @@ class RacasAdapter(var items: List<Raca>) : RecyclerView.Adapter<RacasAdapter.Vi
         return ViewHolder(binding)
     }
 
-    override fun replaceItems(list: List<*>) {
+    override fun replaceItems(items: List<*>) {
         this.items = items as List<Raca>
         notifyDataSetChanged()
     }
 
-    override fun getItemCount() = items.size
+    override fun getItemCount(): Int {
+        return items.size
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(items[position])
