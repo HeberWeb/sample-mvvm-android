28c5f14fdb4fd8a9711ea1c59daa7193413a1439
Heber Nickison Pietrafeza <hpietrafeza@autodoc.com.br>
2019-09-23 11:39:55 -0300

    estruturando classes

---
+++ app/src/main/java/com/example/sample_mvvm_databinding/racas/RacasAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.sample_mvvm_databinding.racas
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.inputmethod.InputBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sample_mvvm_databinding.data.Raca
+
+//class RacasAdapter(var items: List<Raca>) : RecyclerView.Adapter<RacasAdapter.ViewHolder>(), AdapterItemsContract{
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RacasAdapter.ViewHolder {
+//        val inflater = LayoutInflater.from(parent.context)
+//        val binding: InputBinding = InputBinding.in
+//    }
+//
+//    override fun replaceItems(list: List<*>) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    override fun getItemCount(): Int {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    override fun onBindViewHolder(holder: RacasAdapter.ViewHolder, position: Int) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    class ViewHolder(val binding: ) {
+//
+//    }
+//
+//}
--- app/src/main/java/com/example/sample_mvvm_databinding/racas/RacasViewModel.kt
+++ app/src/main/java/com/example/sample_mvvm_databinding/racas/RacasViewModel.kt
@@ -1,8 +1,33 @@
 package com.example.sample_mvvm_databinding.racas
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.*
+import com.example.sample_mvvm_databinding.R
+import com.example.sample_mvvm_databinding.data.Raca
+import com.example.sample_mvvm_databinding.data.RacaDataSource
 
-class RacasViewModel(application: Application) : AndroidViewModel(application) {
+class RacasViewModel(val repository : RacaDataSource, application: Application) :
+    AndroidViewModel(application), LifecycleObserver {
 
+    val racas = MutableLiveData<List<Raca>>().apply { value = emptyList() }
+    val loadingVisibility = MutableLiveData<Boolean>().apply { value = false }
+    val message = MutableLiveData<String>().apply { value = "" }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun load(){
+        loadingVisibility.value = true
+        message.value = ""
+
+        repository.listAll({ items ->
+            racas.value = (items)
+            if (items.isEmpty()){
+                message.value = getApplication<Application>().getString(R.string.sem_registros)
+            }
+
+            loadingVisibility.value = false
+        }, {
+            message.value = getApplication<Application>().getString(R.string.falha_na_listagem)
+            loadingVisibility.value = false
+        })
+    }
 }
